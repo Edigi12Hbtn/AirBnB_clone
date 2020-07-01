a91c3692d70c6a4c2d0a211684303ad220729850
Edison Giraldo A <1470@holbertonschool.com>
2020-07-01 12:38:34 +0000

    Test files

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,7 +1,7 @@
 """Test cases."""
 import unittest
 import pep8
-
+from models.engine.file_storage import FileStorage
 
 class TestCodeFormat(unittest.TestCase):
 
@@ -11,3 +11,25 @@ class TestCodeFormat(unittest.TestCase):
         result = pep8style.check_files(['models/engine/file_storage.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up class before start testing."""
+        cls.FS1 = FileStorage()
+        cls.FS2 = FileStorage()
+
+    def test_docstring(self):
+        """tests class and method documentation."""
+        self.assertIsNotNone(FileStorage.__doc__)
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
+
+    def test_FileStorageinit(self):
+        """tests for initial state of FileStorage instances."""
+        cls = type(self)
+
+        self.assertIsNot(cls.FS1, cls.FS2)
+        self.assertIsInstance(cls.FS1, FileStorage)
+        self.assertIsInstance(cls.FS2, FileStorage)
